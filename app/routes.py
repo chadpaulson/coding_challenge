3ec3b12bfc3bc6c136766692c2170f7fbb855818
Chad Paulson <chad.paulson@gmail.com>
2020-10-24 19:33:14 -0500

    Adds /profile/<username> route and basic API implementation to routes.

--- app/routes.py
+++ app/routes.py
@@ -3,9 +3,22 @@ import logging
 import flask
 from flask import Response
 
+from api import ProfileData, ProfileNotAvailable
+
 app = flask.Flask("user_profiles_api")
 logger = flask.logging.create_logger(app)
 logger.setLevel(logging.INFO)
+
+
+@app.route('/profile/<username>', methods=['GET'])
+def profile(username):
+    profile = ProfileData(username=username)
+    try:
+        profile_data = profile.fetch_profile_data()
+    except ProfileNotAvailable:
+        profile_data = {'error': 'Could not find profile.'}
+
+    return flask.jsonify(profile_data)
 
 
 @app.route("/health-check", methods=["GET"])
