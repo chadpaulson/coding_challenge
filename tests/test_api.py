3da8bb5ac71d3e0912f9f8e8ceb683140d2b518d
Chad Paulson <chad.paulson@gmail.com>
2020-10-25 18:55:52 -0500

    Adds tests.

---
+++ tests/test_api.py
@@ -0,0 +1,26 @@
+import unittest
+from unittest import mock
+
+from app.api import ProfileData
+
+from tests.fixtures import bitbucket, github
+
+
+class APITestCase(unittest.TestCase):
+
+    @mock.patch('app.api.ProfileData.load_bitbucket', return_value=bitbucket)
+    @mock.patch('app.api.ProfileData.load_github', return_value=github)
+    def test_one(self, load_bitbucket, load_github):
+        profile = ProfileData(username='mailchimp')
+        data = profile.fetch_profile_data()
+
+        self.assertEqual(data['num_forked_repos'], 1)
+        self.assertEqual(data['num_original_repos'], 11)
+        self.assertEqual(data['num_total_repos'], 12)
+        self.assertEqual(data['watchers'], 86)
+        self.assertEqual(data['languages']['php'], 2)
+        self.assertEqual(data['languages']['python'], 3)
+        self.assertEqual(data['languages']['javascript'], 3)
+        self.assertEqual(data['languages']['ruby'], 2)
+        self.assertEqual(data['languages']['dart'], 1)
+        self.assertEqual(data['languages']['java'], 1)
